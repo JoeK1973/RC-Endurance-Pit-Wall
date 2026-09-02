38c2d4e0681a1fe44718d47da5485382dc1c2715
JK <joe.keaveney@googlemail.com>
2026-09-02 18:01:02 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PitWall from './PitWall.jsx';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><PitWall /></React.StrictMode>
-);
